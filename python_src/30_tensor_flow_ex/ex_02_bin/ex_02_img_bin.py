13589984f2330dca38b33b61d6dd883f14478c1d
sunabove <sunabove@nate.com>
2020-07-08 10:23:32 +0900

    **

--- python_src/30_tensor_flow_ex/ex_02_bin/ex_02_img_bin.py
+++ python_src/30_tensor_flow_ex/ex_02_bin/ex_02_img_bin.py
@@ -16,9 +16,9 @@ img_path = '../data_opencv_sample/messi5.jpg'
 img = cv2.imread( img_path, cv2.IMREAD_COLOR ) #BGR order
 
 # 이미지 높이, 넓이, 채널수 획득 
-height = img.shape[0]
-width = img.shape[1]
-channel_no = img.shape[1]
+height      = img.shape[0]
+width       = img.shape[1]
+channel_no  = img.shape[2]
 
 print( "Image path: %s" % img_path )
 print( "Image widh: %s, height: %s, channel: %s" % (width,height,channel_no ) )
@@ -57,22 +57,49 @@ pass
 
 # RGB -> GrayScale 변환 공식
 # L = 0.299R + 0.587G + 0.114B
-gray = np.empty( ( height, width ) ) 
+gray = np.empty( ( height, width ), dtype='uint8') 
 
 for y, row in enumerate( gray ) :
     for x, _ in enumerate( row ) :
-        l = 0.299*r_channel[y][x] + 0.587*g_channel[y][x] + 0.114*b_channel[y][x]
-        gray[y][x] = l
+        gs = 0.299*r_channel[y][x] + 0.587*g_channel[y][x] + 0.114*b_channel[y][x]
+        gs = (int)( gs )
+        gray[y][x] = gs
+    pass
+pass
+
+if 0 : 
+    #print( gray )
+    #plt.imshow( gray, cmap='gray', vmin=0, vmax=255)
+    plt.imshow( gray, cmap='gray' )
+    plt.title( "GrapyScale" )
+    plt.colorbar()
+    plt.show()
+pass
+
+# histogram 생성 
+print( "hostogram" )
+histogram = np.zeros( width, dtype=int)
+
+for y, row in enumerate( gray ) : 
+    for x, gs in enumerate( row ) :
+        histogram[ gs ] += 1
     pass
 pass
 
 if 1 : 
-    print( gray )
-    #plt.imshow( gray, cmap='gray', vmin=0, vmax=255)
-    plt.imshow( gray, cmap='gray' )
-    plt.colorbar()
+    y_pos = histogram
+    x_pos = [i for i, _ in enumerate(histogram) ]
+    
+    0 and print( "x_post = %s" % x_pos )
+
+    plt.bar ( x_pos, y_pos, align='center', alpha=1.0)
+    plt.ylabel( 'Count' )
+    plt.ylabel( 'GrayScale' )
+    plt.title( 'Histogram' ) 
+
     plt.show()
-pass
+pass 
+#-- histogram 생성
 
 if 0 : 
     cv2.imshow('image',img)
