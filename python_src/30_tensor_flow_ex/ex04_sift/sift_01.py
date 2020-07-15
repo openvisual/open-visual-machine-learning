bdb3f1bafacdab528e7fcd349917a7b6ae7673a2
openvisual <sunabove@gmail.com>
2020-07-15 11:12:05 +0900

    **

---
+++ python_src/30_tensor_flow_ex/ex04_sift/sift_01.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+import os
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+print( "OpenCV version : %s" % cv.__version__ )
+
+print( "Pwd 1: %s" % os.getcwd())
+# change working dir to current file
+dirname = os.path.dirname(__file__)
+dirname and os.chdir( dirname )
+dirname and print( "Pwd 2: %s" % os.getcwd())
+
+img = cv.imread('../data_opencv_sample/home.jpg' )
+
+gray= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+sift = cv.xfeatures2d.SIFT_create()
+
+kp = sift.detect(gray, None)
+
+print( "keypoints: " , kp )
+
+img = cv.drawKeypoints( gray, kp, img )
+
+cv.imwrite('sift_keypoints.jpg',img)
+
+cv.imshow( "SIFT", img )
+
+cv.waitKey(0)  
+  
+cv.destroyAllWindows() 
+
+# end
