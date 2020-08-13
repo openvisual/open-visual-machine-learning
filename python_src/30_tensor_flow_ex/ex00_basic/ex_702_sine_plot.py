4792a5bf42002f86b07107b694c624cad84b2673
openvisual <openvisual@gmail.com>
2020-08-14 08:34:30 +0900

    사인 그라프 그리기

--- python_src/30_tensor_flow_ex/ex00_basic/ex_702_sine_plot.py
+++ python_src/30_tensor_flow_ex/ex00_basic/ex_702_sine_plot.py
@@ -14,6 +14,8 @@ log.basicConfig(
 from math import pi
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter, MultipleLocator
+
 import matplotlib.colors as mcolors
 import numpy as np
 
@@ -27,8 +29,11 @@ ax.plot( x , y )
 ax.set_xlim(0, 2*pi)
 
 ax.set_xticks( [ 0, pi, 2*pi ] )
-ax.xaxis.set_major_formatter(matplotlib.ticker.FormatStrFormatter('%g $\pi$'))
-#ax.xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(base=1.0))
+#x_labels = ['$0$', r'$\pi/4$', r'$\pi/2$', r'$3\pi/4$', r'$\pi$', r'$5\pi/4$', r'$3\pi/2$', r'$7\pi/4$', r'$2\pi$']
+#ax.set_xticklabels(x_labels)
+ax.set_yticks( [ -1, 0, 1] )
+ax.xaxis.set_major_formatter(FuncFormatter( lambda val,pos: f'{val/pi:.1f} $\pi$' if val !=0 else '0'))
+ax.xaxis.set_major_locator( MultipleLocator(base=pi/2))
 
 plt.grid( 1 )
 plt.show()
