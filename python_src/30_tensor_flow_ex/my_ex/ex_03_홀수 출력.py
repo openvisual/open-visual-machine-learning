c5160728916d696551e1d03ba56f116c8fcb5b97
openvisual <openvisual@gmail.com>
2020-08-20 21:13:16 +0900

    1 ~ 100 사이의 홀수를 모두 출력하세요.

---
+++ python_src/30_tensor_flow_ex/my_ex/ex_03_홀수 출력.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# 1 ~ 100 사이의 홀수를 모두 출력하세요.
+
+# for 문을 사용하는 것을 권장합니다.
+print( "홀수 출력하기")
+
+for i in range( 1, 101 ) :
+    if i%2 == 1 :
+        print( i )
+    pass
+pass
+
+print( "Good bye!")
