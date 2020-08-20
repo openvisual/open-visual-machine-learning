f2b1a4d86d3e40585a450072a1fb8b64e1c61fb3
openvisual <openvisual@gmail.com>
2020-08-20 21:02:59 +0900

    1 ~ 100 까지 출력하기

---
+++ python_src/30_tensor_flow_ex/my_ex/ex_00_print_100.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# 1 ~ 100 까지의 모든 정수들의 출력하세요.
+
+# for 문을 사용하는 것을 권장합니다.
+print( "Hello")
+for i in range( 1, 101 ) :
+    print( i )
+pass
+
+# while 문은 잘 사용하지 않습니다.
+print( "Goog morning!")
+i = 0
+while i < 101 :
+    print( i )
+    i += 1
+pass
--- python_src/30_tensor_flow_ex/my_ex/ex_06_prime_sum.py
+++ python_src/30_tensor_flow_ex/my_ex/ex_06_prime_sum.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+
+# 1 ~ 100 사이의 모든 소수들의 합을 구하세요.
+
 print( "Hello" )
+
 prime_numbers = []
 
 for number in range(2, 100) :
