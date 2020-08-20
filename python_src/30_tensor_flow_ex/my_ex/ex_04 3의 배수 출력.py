cc462c605c7c83d460184e1dba99c6c219a9c1e5
openvisual <openvisual@gmail.com>
2020-08-20 21:14:45 +0900

    1 ~ 100 사이의 3의 배수를 모두 출력하세요.

---
+++ python_src/30_tensor_flow_ex/my_ex/ex_04 3의 배수 출력.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# 1 ~ 100 사이의 3의 배수를 모두 출력하세요.
+
+# for 문을 사용하는 것을 권장합니다.
+print( "3의 배수")
+
+for i in range( 1, 101 ) :
+    if i%3 == 0 :
+        print( i )
+    pass
+pass
+
+print( "Good bye!")
