cf86ec8e22f3b6c31842af95f70ecc029cefcaf6
openvisual <openvisual@gmail.com>
2020-08-30 17:13:10 +0900

    한글 자모 분리 병합 테스트

--- python_src/30_tensor_flow_ex/hangul/test_hangul.py
+++ python_src/30_tensor_flow_ex/hangul/test_hangul.py
@@ -6,6 +6,18 @@
 
 from han_utils.hangul_utils import split_syllable_char, split_syllables, join_jamos
 
-print(split_syllable_char(u"안"))
+print(split_syllable_char("안"))
 
-print(split_syllables(u"안녕하세요"))
+print(split_syllables("안녕하세요"))
+
+sentence = "앞 집 팥죽은 붉은 팥 풋팥죽이고, 뒷집 콩죽은 햇콩 단콩 콩죽.우리 집 깨죽은 검은 깨 깨죽인데 사람들은 햇콩 단콩 콩죽 깨죽 죽먹기를 싫어하더라."
+
+s = split_syllables(sentence)
+
+print( "s = ", s )
+
+s2 = join_jamos(s)
+
+print( "s2 = ", s2 )
+
+print( sentence == s2 )
