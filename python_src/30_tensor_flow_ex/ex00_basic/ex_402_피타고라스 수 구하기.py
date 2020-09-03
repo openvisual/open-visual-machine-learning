42fe8c1142c0c1f7a0aa70e21bd99a23c4dce717
openvisual <openvisual@gmail.com>
2020-09-03 22:31:48 +0900

    Update ex_402_피타고라스 수 구하기.py

--- python_src/30_tensor_flow_ex/ex00_basic/ex_402_피타고라스 수 구하기.py
+++ python_src/30_tensor_flow_ex/ex00_basic/ex_402_피타고라스 수 구하기.py
@@ -1,8 +1,11 @@
 # -*- coding:utf-8 -*-
 
-to = 101
+to = 1000 + 1
 it_count = 0
 idx = 1
+
+length_list = []
+area_list = []
 for a in range(3, to ) :
     for b in range(a + 1, to):
         for c in range( b + 1, to):
@@ -10,6 +13,9 @@ for a in range(3, to ) :
             if a < b < c and a*a + b*b == c*c :
                 length = a + b + c
                 area = int( a*b/2 )
+
+                length_list.append( length )
+                area_list.append( area )
                 print( f"[{idx:d}] {a}, {b}, {c} , length={length:,d}, area={area:,d}" )
                 idx += 1
             pass
@@ -18,3 +24,17 @@ for a in range(3, to ) :
 pass
 
 print( f"실행 횟수 : {it_count:,d}")
+
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import numpy as np
+
+x = range( len( area_list ) )
+
+plt.bar( x, length_list, color="green" , label="Area" )
+plt.plot( x, area_list, color="blue" , label="Length" )
+
+plt.legend()
+
+plt.show()
+pass
