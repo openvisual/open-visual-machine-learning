a815a638cd5a4a9661a4a7dadf476ed9ece10c51
openvisual <openvisual@gmail.com>
2020-09-11 22:51:01 +0900

    **

--- python_src/30_tensor_flow_ex/ex00_basic/ex_410_pattern_04.py
+++ python_src/30_tensor_flow_ex/ex00_basic/ex_410_pattern_04.py
@@ -1,8 +1,16 @@
 # -*- coding:utf-8 -*-
 
 for i in range( 9 + 1 ):
-    count = i + 1 if i < 5 else 9 - i
-    pattern = (" * "*count).center( 13 )
+    count = ( i + 1 ) if i < 5 else 9 - i
+
+    '''
+    count = i + 1
+    if i >= 5:
+        count = 9 - i
+    pass
+    '''
+
+    pattern = ( " * "*count ).center( 13 )
 
     print( pattern )
 pass
