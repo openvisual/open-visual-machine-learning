e238826b45edab2264f7b66aa340e1089fc2c442
openvisual <openvisual@gmail.com>
2020-09-11 22:21:39 +0900

    **

---
+++ python_src/30_tensor_flow_ex/ex00_basic/ex_410_pattern_03.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+for i in range( 9 + 1 ):
+    if i <= 5 :
+        pattern = " * " * i
+    else :
+        pattern = " * " * ( 10 - i )
+    pass
+
+    print( pattern )
+pass
