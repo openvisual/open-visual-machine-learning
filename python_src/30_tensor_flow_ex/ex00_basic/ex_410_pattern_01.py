a24f5eaf3a3d5450a129a53e35e7312c44dc4f25
openvisual <openvisual@gmail.com>
2020-09-11 22:14:20 +0900

    **

--- python_src/30_tensor_flow_ex/ex00_basic/ex_410_pattern_01.py
+++ python_src/30_tensor_flow_ex/ex00_basic/ex_410_pattern_01.py
@@ -1,17 +1,17 @@
 # -*- coding:utf-8 -*-
 
-for i in range(1, 6):
-    for j in range(i):
-        print(i, end = '')
+for i in range( 0, 4 + 1, 1 ):
     pass
-    print()
+pass
+
+for i in range( 5 ):
+    pattern = f"{i+1}"*(i+1)
+    print( pattern )
 pass
 
 print()
-# 수정
-for i in range( 5 ):
-    pattern = f"{i+1}"*(i+1)
-    #pattern *= (i + 1)
-    #pattern = pattern*(i+1)
+
+for i in range( 1, 5 + 1 ):
+    pattern = f"{i}"*i
     print( pattern )
 pass
---
+++ python_src/30_tensor_flow_ex/ex00_basic/ex_410_pattern_02.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+
+for i in range( 5 ):
+    pattern = " * "*(5 - i)
+    print( pattern )
+pass
