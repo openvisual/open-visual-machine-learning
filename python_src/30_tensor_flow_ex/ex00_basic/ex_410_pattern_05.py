55779da2189fe363cb2699822f9ed4e9f8ebd579
openvisual <openvisual@gmail.com>
2020-09-11 23:07:16 +0900

    Update ex_410_pattern_05.py

--- python_src/30_tensor_flow_ex/ex00_basic/ex_410_pattern_05.py
+++ python_src/30_tensor_flow_ex/ex00_basic/ex_410_pattern_05.py
@@ -1,10 +1,9 @@
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 
 for i in range( 9 + 1 ):
     count = ( i + 1 ) if i < 5 else 9 - i
 
-    pattern = ( " *" * count ).center( 10 )
-    pattern *= 5
+    pattern = ( " *" * count ).center( 10 )*5
 
     print( pattern )
 pass
