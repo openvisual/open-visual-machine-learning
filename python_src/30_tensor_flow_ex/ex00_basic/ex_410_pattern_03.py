35f6b9e9ecaa3cdf02dadb6437c0c5447d15a8e7
openvisual <openvisual@gmail.com>
2020-09-12 08:29:00 +0900

    **

--- python_src/30_tensor_flow_ex/ex00_basic/ex_410_pattern_03.py
+++ python_src/30_tensor_flow_ex/ex00_basic/ex_410_pattern_03.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
-for i in range( 1, 10 + 1 ):
-    if i <= 5 :
+for i in range( 1, 10 ):
+    if i < 6 :
         pattern = " * " * i
     else :
         pattern = " * " * ( 10 - i )
